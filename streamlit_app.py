87b795cee9a0f4ea20263ff9cba821a83f577481
thodemcgirth <150072712+thodemcgirth@users.noreply.github.com>
2024-06-01 16:03:22 +0100

    Update streamlit_app.py
    added emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ from pathlib import Path
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP Dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='Theo's First Dashboard',
+    page_icon=':https://i.kym-cdn.com/entries/icons/original/000/046/628/tony_gyatt.jpg:', # This is an emoji shortcode. Could be a URL too.
 )
 
 # -----------------------------------------------------------------------------
